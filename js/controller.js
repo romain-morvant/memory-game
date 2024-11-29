ce218cb6452c4e8299d13c7ebeb42e5668ca07d3
Romain Morvant <romainmorvantdev@gmail.com>
2024-11-29 17:08:08 +0100

    Jeu fonctionnel

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,70 @@ import Elements from "./elements.js";
 class Controller {
   constructor(cardsNumber) {
     this.elements = new Elements(cardsNumber);
+    this.prevCard;
+    this.clickCard();
+  }
+
+  clickCard() {
+    // Récupération des cartes et du nombre de tentatives
+    const { attempts, cards } = this.elements;
+    cards.forEach((card) => {
+      // Incrémentation de la valeur 'click'
+      card.addEventListener("click", () => {
+        attempts.click++;
+        card.classList.add("change");
+        // Si deux cartes sont retournées (ont la classe change)
+        if (attempts.click === 2) {
+          cards.forEach((card) => {
+            // Ajout de la classe 'pause' aux cartes
+            card.classList.add("pause");
+            setTimeout(() => {
+              card.classList.remove("pause");
+              // Le tout après 1000ms => 1s
+            }, 1000);
+          });
+          // Si la carte cliquée précédemment est la même que celle cliquée à l'instant
+          if (this.prevCard.dataset.index === card.dataset.index) {
+            // Incrémentation de la valeur 'correct'
+            attempts.correct++;
+            // Ajout de la classe 'stop' à la carte
+            card.classList.add("stop");
+            // Ajout de la classe 'stop' à la carte précédemment cliquée
+            this.prevCard.classList.add("stop");
+          }
+          // Si la carte cliquée précédemment n'est pas la même que celle cliquée à l'instant
+          else {
+            // Incrémentation de la valeur 'wrong'
+            attempts.wrong++;
+            setTimeout(() => {
+              // Retrait de la classe 'change' de la carte
+              card.classList.remove("change");
+              // Retrait de la classe 'change' de la carte précédemment cliquée
+              this.prevCard.classList.remove("change");
+              // Le tout après 1000ms => 1s
+            }, 1000);
+          }
+          // Remise a 0 du compteur de clicks
+          attempts.click = 0;
+          this.endGame(attempts);
+          console.log(attempts.correct, attempts.wrong);
+        } else {
+          this.prevCard = card;
+        }
+      });
+    });
+  }
+
+  endGame({ correct }) {
+    const { cardsNumber, modal, modalBtn, wrong, attempts } = this.elements;
+    if (correct === cardsNumber / 2) {
+      console.log("Bien ouèj!");
+      modal.style.cssText = "visibility: visible; opacity:1;";
+      wrong.textContent = attempts.wrong;
+      modalBtn.onclick = () => location.reload();
+    } else {
+      console.log("Pas encore :/");
+    }
   }
 }
 
--- js/elements.js
+++ js/elements.js
@@ -2,11 +2,68 @@ class Elements {
   constructor(cardsNumber) {
     this.cardsNumber = cardsNumber;
     this.playground = document.querySelector(".playground");
+    this.cardsIndex = [];
+    this.doubleIndex();
     this.createCard();
+
+    this.cards = document.querySelectorAll(".card");
+    this.attempts = {
+      correct: 0,
+      wrong: 0,
+      click: 0,
+    };
+
+    this.modal = document.querySelector(".modal");
+    this.modalBtn = document.querySelector(".modal-btn");
+    this.wrong = document.querySelector(".wrong");
   }
 
+  // Mélange des index des images avant disposition
+  shuffleIndexes(array) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  // Doublage de l'index histoire d'avoir des paires d'images
+  doubleIndex() {
+    for (let i = 1; i <= this.cardsNumber; i++) {
+      i <= this.cardsNumber / 2
+        ? this.cardsIndex.push(i)
+        : this.cardsIndex.push(i - this.cardsNumber / 2);
+    }
+
+    // console.log(this.cardsIndex);
+  }
+
+  // Création de la carte
   createCard() {
-    console.log(this.cardsNumber);
+    // console.log(this.cardsNumber);
+
+    // Définition du nombre de lignes dépendant du nombre choisi au dessus.
+    this.playground.style.gridTemplateRows = `repeat(${Math.sqrt(
+      this.cardsNumber
+    )}, 1fr)`;
+    // Définition du nombre de colonnes dépendant du nombre choisi au dessus.
+    this.playground.style.gridTemplateColumns = `repeat(${Math.sqrt(
+      this.cardsNumber
+    )}, 1fr)`;
+
+    // Mélange des index juste avant de créer la carte
+    this.shuffleIndexes(this.cardsIndex).forEach((index) => {
+      // Création de la variable card qui est une div.
+      const card = document.createElement("div");
+      // Ajout de la classe 'card' à la div.
+      card.classList.add("card");
+      // Ajout de l'attribut 'data-index' à la div card.
+      card.setAttribute("data-index", index);
+
+      // Création de la variable img qui est un élément img.
+      const img = document.createElement("img");
+      // Définition de la source de l'élément img.
+      img.src = `images/icon-${index}.png`;
+      // Ajout de l'image à la carte.
+      card.append(img);
+      this.playground.append(card);
+    });
   }
 }
 
